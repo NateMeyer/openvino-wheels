3523192bddf14d0eec3bf5c8e35354fa0165060b
Nate Meyer <nate.devel@gmail.com>
2023-07-27 09:41:23 -0400

    Updates for aarch64

--- arm_cpu_plugin_build.sh
+++ arm_cpu_plugin_build.sh
@@ -1,22 +1,34 @@
 #!/bin/sh
 
-set -x
+set -eux
 
 BUILD_JOBS=${BUILD_JOBS:-$(nproc)}
 BUILD_TYPE=${BUILD_TYPE:-Release}
 UPDATE_SOURCES=${UPDATE_SOURCES:-clean}
-WITH_OMZ_DEMO=${WITH_OMZ_DEMO:-ON}
+WITH_OPENCV=${WITH_OPENCV:-ON}
+WITH_OMZ_DEMO=${WITH_OMZ_DEMO:-OFF}
 
-DEV_HOME=`pwd`
-ONETBB_HOME=$DEV_HOME/oneTBB
-OPENCV_HOME=$DEV_HOME/opencv
-OPENVINO_HOME=$DEV_HOME/openvino
-OPENVINO_CONTRIB=$DEV_HOME/openvino_contrib
-ARM_PLUGIN_HOME=$OPENVINO_CONTRIB/modules/arm_plugin
-OMZ_HOME=$DEV_HOME/open_model_zoo
-STAGING_DIR=$DEV_HOME/armcpu_package
+DEV_HOME="$(pwd)"
+ONETBB_HOME="$DEV_HOME/oneTBB"
+OPENCV_HOME="$DEV_HOME/opencv"
+OPENVINO_HOME="$DEV_HOME/openvino"
+OPENVINO_CONTRIB="$DEV_HOME/openvino_contrib"
+ARM_PLUGIN_HOME="$OPENVINO_CONTRIB/modules/arm_plugin"
+OMZ_HOME="$DEV_HOME/open_model_zoo"
+STAGING_DIR="$DEV_HOME/armcpu_package"
+
+ONETBB_BUILD="$DEV_HOME/oneTBB/build"
+OPENCV_BUILD="$DEV_HOME/opencv/build"
+OPENVINO_BUILD="$DEV_HOME/openvino/build"
+OMZ_BUILD="$DEV_HOME/open_model_zoo/build"
+
 OV_PATCH_DIR=/patches
 
+
+# Variables needed by build scripts
+export BUILD_PYTHON=$DEV_HOME/host_python
+export INSTALL_PYTHON=$DEV_HOME/target_python
+export NUM_PROC=$(nproc)
 
 
 fail()
@@ -27,8 +39,8 @@ fail()
     fi
     retval=$1
     shift
-    echo $@
-    exit $retval
+    echo "$@"
+    exit "$retval"
 }
 
 cloneSrcTree()
@@ -38,7 +50,7 @@ cloneSrcTree()
     SRCURL=$1
     shift
     while [ $# -gt 0 ]; do
-        git clone --recurse-submodules --shallow-submodules --depth 1 --branch=$1 $SRCURL $DESTDIR && return 0
+        git clone --recurse-submodules --shallow-submodules --depth 1 --branch="$1" "$SRCURL" "$DESTDIR" && return 0
         shift
     done
     return 1
@@ -48,176 +60,182 @@ checkSrcTree()
 {
     [ $# -lt 3 ] && fail
 
-    if ! [ -d $1 ]; then
+    if ! [ -d "$1" ]; then
         echo "Unable to detect $1"
         echo "Cloning $2..."
-        cloneSrcTree $@ || fail 3 "Failed to clone $2. Stopping"
+        cloneSrcTree "$@" || fail 3 "Failed to clone $2. Stopping"
     else
         echo "Detected $1"
         echo "Considering it as source directory"
         if [ "$UPDATE_SOURCES" = "reload" ]; then
             echo "Source reloading requested"
             echo "Removing existing sources..."
-            rm -rf $1 || fail 1 "Failed to remove. Stopping"
+            rm -rf "$1" || fail 1 "Failed to remove. Stopping"
             echo "Cloning $2..."
-            cloneSrcTree $@ || fail 3 "Failed to clone $2. Stopping"
-        elif [ -d $1/build ]; then
+            cloneSrcTree "$@" || fail 3 "Failed to clone $2. Stopping"
+        elif [ -d "$1/build" ]; then
             echo "Build directory detected at $1"
             if [ "$UPDATE_SOURCES" = "clean" ]; then
                 echo "Cleanup of previous build requested"
                 echo "Removing previous build results..."
-                rm -rf $1/build || fail 2 "Failed to cleanup. Stopping"
+                rm -rf "$1/build" || fail 2 "Failed to cleanup. Stopping"
             fi
         fi
     fi
     return 0
 }
 
-
-
-#Prepare sources
+# Prepare sources
 if ! [ -d $OPENVINO_HOME ] || [ "$UPDATE_SOURCES" = "reload" ]; then
     NEED_PATCH=true;
 else
     NEED_PATCH=false;
 fi
 
-checkSrcTree $ONETBB_HOME https://github.com/oneapi-src/oneTBB.git master
-checkSrcTree $OPENCV_HOME https://github.com/opencv/opencv.git 4.x
-checkSrcTree $OPENVINO_HOME https://github.com/openvinotoolkit/openvino.git 2022.3.1
-checkSrcTree $OPENVINO_CONTRIB https://github.com/openvinotoolkit/openvino_contrib.git 2022.3.0
+checkSrcTree "$ONETBB_HOME" https://github.com/oneapi-src/oneTBB.git master
+if [ "$WITH_OPENCV" = "ON" ]; then
+    checkSrcTree "$OPENCV_HOME" https://github.com/opencv/opencv.git 4.x
+fi
+checkSrcTree "$OPENVINO_HOME" https://github.com/openvinotoolkit/openvino.git 2022.3.1
+checkSrcTree "$OPENVINO_CONTRIB" https://github.com/openvinotoolkit/openvino_contrib.git releases/2022/3
 if [ "$WITH_OMZ_DEMO" = "ON" ]; then
-    checkSrcTree $OMZ_HOME https://github.com/openvinotoolkit/open_model_zoo.git master
+    checkSrcTree "$OMZ_HOME" https://github.com/openvinotoolkit/open_model_zoo.git releases/2022/3
 fi
 
 # Apply Openvino patches
 if [ $NEED_PATCH = true ]; then
     patch -p 1 -d $OPENVINO_HOME -i /patches/vpu-wheel.patch
+    patch -p 1 -d $OPENVINO_HOME -i /patches/numpy-version.patch
 fi
 
-#cleanup package destination folder
-[ -e $STAGING_DIR ] && rm -rf $STAGING_DIR
-mkdir -p $STAGING_DIR
+# python variables
+python_executable="$(which python3)"
+python_min_ver=$($python_executable -c "import sys; print(str(sys.version_info[1]))")
+python_library_name=$($python_executable -c "import sysconfig as s; print(str(s.get_config_var(\"LDLIBRARY\")))")
+python_library_dir=$($python_executable -c "import sysconfig as s; print(str(s.get_config_var(\"LIBDIR\")))")
+python_library="$python_library_dir/$python_library_name"
+python_inc_dir=$($python_executable -c "import sysconfig as s; print(str(s.get_config_var(\"INCLUDEPY\")))")
+numpy_inc_dir=$($python_executable -c "import numpy; print(numpy.get_include())")
 
+if [ -n "$TOOLCHAIN_DEFS" ]; then
+    export CMAKE_TOOLCHAIN_FILE="$OPENVINO_HOME/cmake/$TOOLCHAIN_DEFS"
+    # use cross-compiled binaries
+    pymalloc=""
+    [ "$python_min_ver" -lt "8" ] && pymalloc="m"
+    python_library="/opt/python3.${python_min_ver}_arm/lib/libpython3.${python_min_ver}${pymalloc}.so"
+    python_inc_dir="/opt/python3.${python_min_ver}_arm/include/python3.${python_min_ver}${pymalloc}"
+fi
 
-#Build oneTBB
-mkdir -p $ONETBB_HOME/build && cd $ONETBB_HOME/build && \
-cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-    -DCMAKE_TOOLCHAIN_FILE="$OPENVINO_HOME/cmake/$TOOLCHAIN_DEFS" \
-    -DCMAKE_INSTALL_PREFIX=$STAGING_DIR/extras/oneTBB .. && \
-cmake --build . -j $BUILD_JOBS&& \
-cmake --install . && \
-echo export TBB_DIR=\$INSTALLDIR/extras/oneTBB/cmake/TBB > $STAGING_DIR/extras/oneTBB/setupvars.sh && \
-echo export LD_LIBRARY_PATH=\$INSTALLDIR/extras/oneTBB/lib:\$LD_LIBRARY_PATH >> $STAGING_DIR/extras/oneTBB/setupvars.sh && \
-cd $DEV_HOME || fail 11 "oneTBB build failed. Stopping"
+# cleanup package destination folder
+[ -e "$STAGING_DIR" ] && rm -rf "$STAGING_DIR"
+mkdir -p "$STAGING_DIR"
 
-# Install and build dependencies
-$OPENVINO_HOME/install_build_dependencies.sh
-$OPENVINO_CONTRIB/modules/arm_plugin/scripts/install_build_dependencies.sh
+# Build oneTBB
+cmake -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
+    -DCMAKE_CXX_FLAGS="-Wno-error=attributes" \
+    -DCMAKE_INSTALL_PREFIX="$ONETBB_BUILD/install" \
+    -DTBB_TEST=OFF \
+    -S "$ONETBB_HOME" \
+    -B "$ONETBB_BUILD" && \
+cmake --build "$ONETBB_BUILD" --parallel "$BUILD_JOBS" && \
+cmake --install "$ONETBB_BUILD" && \
+cd "$DEV_HOME" || fail 11 "oneTBB build failed. Stopping"
 
-#Build OpenCV
-mkdir -p $OPENCV_HOME/build && \
-cd $OPENCV_HOME/build && \
-PYTHONVER=`ls /usr/include | grep "python3[^m]*$"` && \
-cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBUILD_LIST=imgcodecs,videoio,highgui,gapi,python3 \
-      -DTBB_DIR="$STAGING_DIR/extras/oneTBB/lib/cmake/TBB" \
-      -DBUILD_opencv_python2=OFF -DBUILD_opencv_python3=ON -DOPENCV_SKIP_PYTHON_LOADER=OFF \
-      -DPYTHON3_LIMITED_API=ON \
-      -DPYTHON3_INCLUDE_PATH=/opt/python3.9_arm/include/python3.9 \
-      -DPYTHON3_LIBRARIES=/opt/python3.9_arm/lib \
-      -DPYTHON3_NUMPY_INCLUDE_DIRS=/usr/local/lib/python3.9/site-packages/numpy/core/include \
-      -D CMAKE_USE_RELATIVE_PATHS=ON \
-      -D CMAKE_SKIP_INSTALL_RPATH=ON \
-      -D OPENCV_SKIP_PKGCONFIG_GENERATION=ON \
-      -D OPENCV_BIN_INSTALL_PATH=bin \
-      -D OPENCV_PYTHON3_INSTALL_PATH=python \
-      -D OPENCV_INCLUDE_INSTALL_PATH=include \
-      -D OPENCV_LIB_INSTALL_PATH=lib \
-      -D OPENCV_CONFIG_INSTALL_PATH=cmake \
-      -D OPENCV_3P_LIB_INSTALL_PATH=3rdparty \
-      -D OPENCV_SAMPLES_SRC_INSTALL_PATH=samples \
-      -D OPENCV_DOC_INSTALL_PATH=doc \
-      -D OPENCV_OTHER_INSTALL_PATH=etc \
-      -D OPENCV_LICENSES_INSTALL_PATH=etc/licenses \
-      -DCMAKE_TOOLCHAIN_FILE="$OPENVINO_HOME/cmake/$TOOLCHAIN_DEFS" \
-      -DWITH_GTK_2_X=OFF \
-      -DOPENCV_ENABLE_PKG_CONFIG=ON \
-      -DPKG_CONFIG_EXECUTABLE=/usr/bin/${ARCH_NAME}-pkg-config \
-      $OPENCV_HOME && \
-make -j$BUILD_JOBS && \
-cmake -DCMAKE_INSTALL_PREFIX=$STAGING_DIR/extras/opencv -P cmake_install.cmake && \
-echo export OpenCV_DIR=\$INSTALLDIR/extras/opencv/cmake > $STAGING_DIR/extras/opencv/setupvars.sh && \
-echo export LD_LIBRARY_PATH=\$INSTALLDIR/extras/opencv/lib:\$LD_LIBRARY_PATH >> $STAGING_DIR/extras/opencv/setupvars.sh && \
-mkdir -p $STAGING_DIR/python/python3 && cp -r $STAGING_DIR/extras/opencv/python/cv2 $STAGING_DIR/python/python3 && \
-cd $DEV_HOME || fail 11 "OpenCV build failed. Stopping"
+# export TBB for later usage in OpenCV / OpenVINO
+export TBB_DIR="$ONETBB_BUILD/install/lib/cmake/TBB/"
+# Build OpenCV
+if [ "$WITH_OPENCV" = "ON" ]; then
+    cmake -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
+        -DBUILD_LIST=imgcodecs,videoio,highgui,gapi,python3 \
+        -DCMAKE_INSTALL_PREFIX="$STAGING_DIR/extras/opencv" \
+        -DBUILD_opencv_python2=OFF \
+        -DBUILD_opencv_python3=ON \
+        -DOPENCV_SKIP_PYTHON_LOADER=OFF \
+        -DPYTHON3_LIMITED_API=ON \
+        -DPYTHON3_EXECUTABLE="$python_executable" \
+        -DPYTHON3_INCLUDE_PATH="$python_inc_dir" \
+        -DPYTHON3_LIBRARIES="$python_library" \
+        -DPYTHON3_NUMPY_INCLUDE_DIRS="$numpy_inc_dir" \
+        -DCMAKE_USE_RELATIVE_PATHS=ON \
+        -DCMAKE_SKIP_INSTALL_RPATH=ON \
+        -DOPENCV_SKIP_PKGCONFIG_GENERATION=ON \
+        -DOPENCV_BIN_INSTALL_PATH=bin \
+        -DOPENCV_PYTHON3_INSTALL_PATH=python \
+        -DOPENCV_INCLUDE_INSTALL_PATH=include \
+        -DOPENCV_LIB_INSTALL_PATH=lib \
+        -DOPENCV_CONFIG_INSTALL_PATH=cmake \
+        -DOPENCV_3P_LIB_INSTALL_PATH=3rdparty \
+        -DOPENCV_SAMPLES_SRC_INSTALL_PATH=samples \
+        -DOPENCV_DOC_INSTALL_PATH=doc \
+        -DOPENCV_OTHER_INSTALL_PATH=etc \
+        -DOPENCV_LICENSES_INSTALL_PATH=etc/licenses \
+        -DWITH_GTK_2_X=OFF \
+        -DOPENCV_ENABLE_PKG_CONFIG=ON \
+        -S "$OPENCV_HOME" \
+        -B "$OPENCV_BUILD" && \
+    cmake --build "$OPENCV_BUILD" --parallel "$BUILD_JOBS" && \
+    cmake --install "$OPENCV_BUILD" && \
+    mkdir -pv "$STAGING_DIR/python/python3" && cp -r "$STAGING_DIR/extras/opencv/python/cv2" "$STAGING_DIR/python/python3" && \
+    cd "$DEV_HOME" || fail 11 "OpenCV build failed. Stopping"
 
-#Build OpenVINO
-# prepare cross-enviroment
-/usr/bin/python3 -m pip install --upgrade pip crossenv setuptools
-/usr/bin/python3 -m crossenv /opt/python3.9_arm/bin/python3.9 /tmp/cross_venv
-. /tmp/cross_venv/bin/activate && \
-cross-python -m pip install wheel && \
-cross-python -m pip install numpy==1.20.0
+    # export OpenCV for later usage in OpenVINO
+    export OpenCV_DIR="$STAGING_DIR/extras/opencv/cmake"
+fi
 
-mkdir -p $OPENVINO_HOME/build && \
-cd $OPENVINO_HOME/build && \
-cmake -DOpenCV_DIR=$STAGING_DIR/extras/opencv/cmake -DENABLE_OPENCV=OFF \
-      -DTBB_DIR="$STAGING_DIR/extras/oneTBB/lib/cmake/TBB" \
-      -DPYTHON_INCLUDE_DIRS="/opt/python3.9_arm/include/python3.9" \
-      -DPYTHON_LIBRARY="/opt/python3.9_arm/lib/libpython3.9.so" \
-      -DENABLE_WHEEL=OFF \
-      -DNGRAPH_ONNX_IMPORT_ENABLE=ON \
-      -DENABLE_TESTS=OFF -DENABLE_FUNCTIONAL_TESTS=OFF -DENABLE_GAPI_TESTS=OFF \
+# Build OpenVINO
+cmake -DENABLE_CPPLINT=OFF \
+      -DENABLE_NCC_STYLE=OFF \
+      -DENABLE_PYTHON=OFF \
+      -DENABLE_TEMPLATE=OFF \
+      -DENABLE_TESTS=OFF \
+      -DENABLE_GAPI_TESTS=OFF \
       -DENABLE_DATA=OFF \
-      -DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath-link,$STAGING_DIR/opencv/lib \
-      -DENABLE_MYRIAD=ON -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-      -DTHREADING=TBB -DENABLE_LTO=ON \
-      -DCMAKE_TOOLCHAIN_FILE="$OPENVINO_HOME/cmake/$TOOLCHAIN_DEFS" \
-      -DARM_COMPUTE_SCONS_JOBS=$BUILD_JOBS \
-      -DIE_EXTRA_MODULES=$ARM_PLUGIN_HOME \
-      -DENABLE_PYTHON=ON -DPYTHON_EXECUTABLE=`which cross-python` \
-      -DENABLE_WHEEL=ON \
-      -DPYTHON_INCLUDE_DIRS=/opt/python3.9_arm/include/python3.9 \
-      -DPYTHON_LIBRARIES=/opt/python3.9_arm/lib \
-      -DPYTHON_LIBRARY=/opt/python3.9_arm/lib/libpython3.9.so \
-      -DPYTHON_MODULE_EXTENSION=".so" \
-      -DPYBIND11_FINDPYTHON=OFF \
-      -DPYBIND11_NOPYTHON=OFF \
-      -DPYTHONLIBS_FOUND=TRUE \
-      -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DENABLE_DATA=OFF \
-      -DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath-link,$STAGING_DIR/opencv/lib \
-      -DCMAKE_TOOLCHAIN_FILE="$OPENVINO_HOME/cmake/$TOOLCHAIN_DEFS" \
-      $OPENVINO_HOME && \
-make -j$BUILD_JOBS && \
-cmake -DCMAKE_INSTALL_PREFIX=$STAGING_DIR -P cmake_install.cmake && \
-ARCHDIR=`ls $OPENVINO_HOME/bin` && \
-deactivate
-cd $DEV_HOME || fail 12 "OpenVINO build failed. Stopping"
+      -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
+      -DARM_COMPUTE_SCONS_JOBS="$BUILD_JOBS" \
+      -DOPENVINO_EXTRA_MODULES="$ARM_PLUGIN_HOME" \
+      -S "$OPENVINO_HOME" \
+      -B "$OPENVINO_BUILD" && \
+cmake --build "$OPENVINO_BUILD" --parallel "$BUILD_JOBS" && \
+cmake --install "$OPENVINO_BUILD" --prefix "$STAGING_DIR" && \
+cd "$DEV_HOME" || fail 12 "OpenVINO build failed. Stopping"
 
-#Open Model Zoo
+# OpenVINO python
+[ "$UPDATE_SOURCES" = "clean" ] && [ -e "$OPENVINO_BUILD/pbuild" ] && rm -rf "$OPENVINO_BUILD/pbuild"
+[ -e "/opt/cross_venv/bin/activate" ] && . /opt/cross_venv/bin/activate
+
+cmake -DOpenVINODeveloperPackage_DIR="$OPENVINO_BUILD" \
+      -DCMAKE_INSTALL_PREFIX="$STAGING_DIR" \
+      -DENABLE_PYTHON=ON \
+      -DENABLE_WHEEL=ON \
+      -S "$OPENVINO_HOME/src/bindings/python" \
+      -B "$OPENVINO_BUILD/pbuild" && \
+cmake --build "$OPENVINO_BUILD/pbuild" --parallel "$BUILD_JOBS" && \
+cmake --install "$OPENVINO_BUILD/pbuild" && \
+cd "$DEV_HOME" || fail 13 "OpenVINO python bindings build failed. Stopping"
+
+# deactivate crossenv
+[ -e "/opt/cross_venv/bin/activate" ] && deactivate
+
+# Open Model Zoo
 if [ "$WITH_OMZ_DEMO" = "ON" ]; then
-  OMZ_DEMOS_BUILD=$OMZ_HOME/build && \
-  mkdir -p $OMZ_DEMOS_BUILD && \
-  cd $OMZ_DEMOS_BUILD && \
-  cmake -DCMAKE_BUILD_TYPE=Release \
+  cmake -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
         -DENABLE_PYTHON=ON \
-        -DPYTHON_EXECUTABLE=/usr/local/bin/python3.9 \
-        -DPYTHON_INCLUDE_DIR="/opt/python3.9_arm/include/python3.9" \
-        -DPYTHON_LIBRARY="/opt/python3.9_arm/lib/libpython3.9.so" \
-        -DCMAKE_TOOLCHAIN_FILE="$OPENVINO_HOME/cmake/$TOOLCHAIN_DEFS" \
-        -DOpenVINO_DIR=$OPENVINO_HOME/build \
-        -DOpenCV_DIR=$OPENCV_HOME/build \
-        $OMZ_HOME/demos && \
-  cmake --build $OMZ_DEMOS_BUILD -- -j$BUILD_JOBS && \
-  cd $DEV_HOME || fail 16 "Open Model Zoo build failed. Stopping"
-  python3 $OMZ_HOME/ci/prepare-openvino-content.py l $OMZ_DEMOS_BUILD && \
-  cp -vr $OMZ_DEMOS_BUILD/dev/. $STAGING_DIR && \
-  find $OMZ_DEMOS_BUILD -type d -name "Release" -exec cp -vr {} $STAGING_DIR/extras/open_model_zoo/demos \; || \
+        -DPYTHON_EXECUTABLE="$python_executable" \
+        -DPYTHON_INCLUDE_DIR="$python_inc_dir" \
+        -DPYTHON_LIBRARY="$python_library" \
+        -DOpenVINO_DIR="$OPENVINO_HOME/build" \
+        -S "$OMZ_HOME/demos" \
+        -B "$OMZ_BUILD" && \
+  cmake --build "$OMZ_BUILD" --parallel "$BUILD_JOBS" && \
+  cd "$DEV_HOME" || fail 16 "Open Model Zoo build failed. Stopping"
+  python3 "$OMZ_HOME/ci/prepare-openvino-content.py" l "$OMZ_BUILD" && \
+  cp -vr "$OMZ_BUILD/dev/." "$STAGING_DIR" && \
+  find "$OMZ_BUILD" -type d -name "Release" -exec cp -vr {} "$STAGING_DIR/extras/open_model_zoo/demos" \; || \
   fail 21 "Open Model Zoo package preparation failed. Stopping"
 fi
 
-#Package creation
-cd $STAGING_DIR && \
+# Package creation
+cd "$STAGING_DIR" && \
 tar -czvf ../OV_${ARCH_NAME}_package.tar.gz ./* || \
 fail 23 "Package creation failed. Nothing more to do"
 
